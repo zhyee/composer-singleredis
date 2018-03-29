190b9eca64e4a87cced65ba28b2409c5cc7a0edb
global <global@example.com>
2018-03-29 12:47:20 +0800

    init

---
+++ singleton/redis/RedisSingleton.php
@@ -0,0 +1,23 @@
+<?php
+namespace singleton;
+
+class RedisSingleton {
+        private static $_redis = NULL;
+
+        private function __construct(){}
+
+    public static function getInstance(){
+        if (self::$_redis == NULL) {
+            $_redis = new Redis();
+            $_redis->connect("127.0.0.1", 6379);
+        }
+        return self::$_redis;
+    }
+    
+        public static function close(){
+                if (self::$_redis !== NULL) {
+                        self::$_redis->close();
+                        self::$_redis = NULL;
+                }
+    }
+}
